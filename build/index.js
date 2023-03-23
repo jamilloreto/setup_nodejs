241d482db73aace0702981de1822503281362e40
Jamil Loreto <jamil20002302@gmail.com>
2023-03-22 22:51:10 -0500

    commit initial

---
+++ build/index.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const app = (0, express_1.default)();
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.get("/", (_, res) => {
+    res.send("TS App is Running");
+});
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`server is running on PORT ${PORT}`);
+});
